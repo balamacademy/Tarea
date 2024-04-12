fcddd436b6a738443a1fc8a905b253e6cf1c1d0c
IsabellaJB <isabellajib5@gmail.com>
2024-04-12 17:37:49 -0600

    tarea

--- 3IntervalHalvingMethod_Graficas.py
+++ 3IntervalHalvingMethod_Graficas.py
@@ -1,70 +1,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-# el -1 hace que cambie el sentido del probelma
 
+# x = np.arange(0.5, 8.5, 0.5)
 def lata(x):
     operacion = ((2 * np.pi) * (x**2) + 500/x)
     return operacion
 
-
+# x = np.arange(2, 3.1, 0.1)
 def caja(x):
     return -1*(((20 - (2 * x)) * (10 - (2 * x))) * (x))
 
-
+# Valores entre 0 o igual a 10
 def funcion1(x):
-    return (x**2) + (54/x)
+    operacion = (x**2) + (54/x)
+    return operacion
 
-
+# Valores entre 0 o igual a 5
 def funcion2(x):
-    return (x**3) + (2*x) - 3
+    operacion = (x**3) + (2*x) - 3
+    return operacion
 
-
+# Valores entre -2.5 o igual a 2.5
 def funcion3(x):
-    return (x**4) + (x**2) - 33
+    operacion = (x**4) + (x**2) - 33
+    return operacion
 
-
+# Valores entre -1.5 o igual a 3
 def funcion4(x):
-    return (3*(x**4)) - (8*(x**3)) - (6*(x**2)) + (12*x)
+    operacion = (3*(x**4)) - (8*(x**3)) - (6*(x**2)) + (12*x) 
+    return operacion
 
 
-def metodo_fase(punto_inicial, delta, funcion):
-    punto_inicial = 0.6
-    k = 0
 
-    x1 = punto_inicial - abs(delta)  
-    x2 = punto_inicial
-    x3 = punto_inicial + abs(delta)
+def intervalMethod(x,error,funcion):
+    a = x[0]
+    b = x[-1]
 
-    fx1 = funcion(x1)
-    fx2 = funcion(x2)
-    fx3 = funcion(x3)
+    Xm = (a+b) / 2
+    
 
-    if fx1 > fx2 > fx3:
-        delta = abs(delta)
-    else:
-        if delta < 0:
-            delta = delta
-        else:
-            delta = -1 * delta
-
+    L = b - a
 
     bandera = 0
 
+
+
     while (bandera != 1):
+        x1 = a + (L/4)
+        x2 = b - (L/4)
 
-        x_anterior = x2
-        f_anterior = funcion(x_anterior)
-        x_nueva = x_anterior + ((2**k) * delta)
-        f_nueva = funcion(x_nueva)
-        k += 1
+        funcionX1 = funcion(x1)
+        funcionX2 = funcion(x2)
+        funcionXm = funcion(Xm)
 
-        if (f_nueva < f_anterior):
-            x2 = x_nueva
+        if funcionX1 < funcionXm:
+            b = Xm
+            Xm = x1
         else:
-            bandera = 1
+            if funcionX2 < funcionXm:
+                a = Xm
+                Xm = x2
+            else:
+                a = x1
+                b = x2 
 
-    return x_anterior, x_nueva
+        L = b - a
+
+        if abs(L) < error:
+            # print(Xm)
+            bandera = 1
+        # else:
+        #     print(x1)
+        #     print(x2)
+        #     print("\n"*3)
+    return x1,x2
+
+
+
+# x = np.arange(0,6,1)
+# error = 10**-3
+
+
+
+# precision
+# error = 0.7
+
 
 
 def plot_function(ax, x_range, y_values, label, color):
@@ -81,23 +103,23 @@ for n_precision in n_precisions:
     x_lata = np.arange(0.5, 8.5, 0.01)
     x_caja = np.arange(2, 3.1, 0.01)
     x_funcion1 = np.arange(0.1, 10, 0.01)
-    # x_funcion2 = np.arange(0.1, 5, 0.01)
+    x_funcion2 = np.arange(0.1, 5, 0.01)
     x_funcion3 = np.arange(-2.5, 2.5, 0.01)
     x_funcion4 = np.arange(-1.5, 3, 0.01)
 
     # Ejecutar la búsqueda exhaustiva y obtener los puntos mínimos para cada función
-    min_lata = metodo_fase(x_lata, n_precision, lata)
-    min_caja = metodo_fase(x_caja,n_precision, caja)
-    min_funcion1 = metodo_fase(x_funcion1, n_precision, funcion1)
-    # min_funcion2 = metodo_fase(x_funcion2, n_precision, funcion2)
-    min_funcion3 = metodo_fase(x_funcion3, n_precision, funcion3)
-    min_funcion4 = metodo_fase(x_funcion4, n_precision, funcion4)
+    min_lata = intervalMethod(x_lata, n_precision, lata)
+    min_caja = intervalMethod(x_caja,n_precision, caja)
+    min_funcion1 = intervalMethod(x_funcion1, n_precision, funcion1)
+    min_funcion2 = intervalMethod(x_funcion2, n_precision, funcion2)
+    min_funcion3 = intervalMethod(x_funcion3, n_precision, funcion3)
+    min_funcion4 = intervalMethod(x_funcion4, n_precision, funcion4)
 
     # Calcular los valores de las funciones en los rangos dados
     y_lata = lata(x_lata)
     y_caja = caja(x_caja)
     y_funcion1 = funcion1(x_funcion1)
-    # y_funcion2 = funcion2(x_funcion2)
+    y_funcion2 = funcion2(x_funcion2)
     y_funcion3 = funcion3(x_funcion3)
     y_funcion4 = funcion4(x_funcion4)
 
@@ -116,7 +138,9 @@ for n_precision in n_precisions:
     plot_points(axs[0, 2], [min_funcion1[0], min_funcion1[1]], [funcion1(min_funcion1[0]), funcion1(min_funcion1[1])], 'Min Función 1', 'red')
     axs[0, 2].set_title(f'Función 1 (n_precision = {n_precision})')
 
-    # plot_function(axs[1, 0], x_funcion2, y_funcion2, 'Función 2', 'purple')g
+    plot_function(axs[1, 0], x_funcion2, y_funcion2, 'Función 2', 'purple')
+    plot_points(axs[1, 0], [min_funcion2[0], min_funcion2[1]], [funcion2(min_funcion2[0]), funcion2(min_funcion2[1])], 'Min Función 2', 'red')
+    axs[1, 0].set_title(f'Función 2 (n_precision = {n_precision})')
 
     plot_function(axs[1, 1], x_funcion3, y_funcion3, 'Función 3', 'brown')
     plot_points(axs[1, 1], [min_funcion3[0], min_funcion3[1]], [funcion3(min_funcion3[0]), funcion3(min_funcion3[1])], 'Min Función 3', 'red')
@@ -128,3 +152,10 @@ for n_precision in n_precisions:
 
     plt.tight_layout()
     plt.show()
+
+
+
+
+
+
+
